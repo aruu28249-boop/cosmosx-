e9ee0745fdfa84b8495490407035703dcd61bb27
Jhalak134 <mittaljhalak524@gmail.com>
2026-06-10 01:45:48 +0530

    fixed

--- data/planets.js
+++ data/planets.js
@@ -1,1 +1,32 @@
-export const planets = [];
+export const PLANETS = [
+  {
+    name: "Earth",
+    color: "#4B9CD3",
+    size: 1.5,
+    orbitRadius: 28,
+    orbitSpeed: 1.0,
+    rotationSpeed: 0.5,
+    distanceFromSun: "149.6 million km",
+    funFact: "Only known planet with life"
+  },
+  {
+    name: "Mars",
+    color: "#C1440E",
+    size: 1.0,
+    orbitRadius: 42,
+    orbitSpeed: 0.53,
+    rotationSpeed: 0.48,
+    distanceFromSun: "227.9 million km",
+    funFact: "Has the tallest volcano in the solar system"
+  },
+  {
+    name: "Jupiter",
+    color: "#C88B3A",
+    size: 3.5,
+    orbitRadius: 72,
+    orbitSpeed: 0.08,
+    rotationSpeed: 1.2,
+    distanceFromSun: "778.5 million km",
+    funFact: "Has 95 known moons"
+  }
+]
